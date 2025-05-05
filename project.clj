39d6b46d211093b7181109eed10bb38815e4d380
Dimitrios Piliouras <jimpil1985@gmail.com>
2025-05-05 20:32:45 +0100

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.jimpil/jte-clj "0.1.1-SNAPSHOT"
+(defproject com.github.jimpil/jte-clj "0.1.1"
   :description "Lightweight Clojure wrapper around JTE (Java Template Engine)"
   :url "https://github.com/jimpil/jte-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
